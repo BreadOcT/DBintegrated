2e680241abc055754689bf69f5e83ba265e4cddc
BreadOcT <punyaokto@gmail.com>
2026-05-31 20:39:42 +0700

    fixed : kelewatan baris akhir

--- server.ts
+++ server.ts
@@ -1389,9 +1389,9 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  // app.listen(PORT, "0.0.0.0", () => {
+  //   console.log(`Server running on http://localhost:${PORT}`);
+  // });
 }
 
 export default app;
